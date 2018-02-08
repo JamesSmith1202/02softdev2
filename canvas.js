a70ab16197c065fc593a844c5a3cfa91eb8b6176
JamesSmith1202 <jsmith@stuy.edu>
2018-02-08 14:52:20 -0500

    :rat: :rat: did the next part :rat: :rat:

--- canvas.js
+++ canvas.js
@@ -1,10 +1,16 @@
 var canvas = document.getElementById("myCanvas");
 var stopButton = document.getElementById("stop");
 var clearButton = document.getElementById("clear");
+var growButton = document.getElementById("grow");
+var dvdButton = document.getElementById("dvd");
 var context = canvas.getContext('2d');
 var frame;
 var increment = 1;
 var radius = 1;
+var dx = 2;
+var dy = 2;
+var x = 20 + Math.random() * (canvas.width - 40);
+var y = 20 +  Math.random() * (canvas.height - 40);
 
 context.lineWidth = 5; 
 context.fillStyle = "red";
@@ -17,9 +23,11 @@ var clear = function(){
     context.clearRect(0, 0, canvas.width, canvas.height);
     radius = 1;
     increment = 1;
+    x = 20 + Math.random() * (canvas.width - 40);
+    y = 20 +  Math.random() * (canvas.height - 40);
 }
 
-var animate = function(){
+var animateGrow = function(){
     var checkBounds = function(){
         if (radius == canvas.width/2 || radius == canvas.height/2 || radius == 0){
             increment *= -1;
@@ -40,6 +48,32 @@ var animate = function(){
     drawCircle();
 }
 
-canvas.addEventListener("click", animate);
+var animateDVD = function(){
+    var checkBounds = function(){
+        if (x-20 <= 0 || x + 20 >= canvas.width){
+            dx *=-1;
+        } 
+        if(y - 20 <= 0 | y + 20 >= canvas.height){
+            dy *= -1;
+        }
+    }
+
+    var drawCircle = function(){
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();
+        context.arc(x, y, 20, 0, 2 * Math.PI, false);
+        context.fill();     
+        context.stroke();
+        frame = window.requestAnimationFrame(drawCircle);
+        x += dx;
+        y += dy;
+        checkBounds();
+    }
+    stop();
+    drawCircle();
+}
+
+growButton.addEventListener("click", animateGrow);
+dvdButton.addEventListener("click", animateDVD);
 stopButton.addEventListener("click", stop);
 clearButton.addEventListener("click", clear);
